ce5bc5869a49b1d7ca75d8be1a311567e99e5d3a
ifqeme <romatkachenko3577@gmail.com>
2023-04-15 18:31:10 +0300

    Розроблено функцію s_calculation

---
+++ lab08/prj/ModulesTkachenko/main.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+
+const double pi = 3.14159;
+const double e = 2.71828;
+
+float s_calculation(float x, float y,float z)
+{
+    float result = 0.5*((pow(y,2)+ 2*z)/sqrt(7*pi + x)) - sqrt(pow(e,abs(x))+ (sqrt(abs(y-z)))/sin(z*y));
+    return result;
+}
