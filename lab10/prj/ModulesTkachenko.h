2eb777f841c21963d80dafff0e6ba2981636b663
ifqeme <romatkachenko3577@gmail.com>
2023-05-15 01:22:02 +0300

    Додано заголовковий файл ModulesTkachenko

---
+++ lab10/prj/ModulesTkachenko.h
@@ -0,0 +1,14 @@
+#ifndef MODULESTKACHENKO_H_INCLUDED
+#define MODULESTKACHENKO_H_INCLUDED
+
+using namespace std;
+
+float s_calculation(float x, float y,float z);
+string inputFileTxt(string inputFilename,string sentence);
+bool authorInfo(std::string outputFileName);
+string language(string outputFileName, string text);
+bool article_62(string inputFileText);
+float sCalculationInFile(string outputFileName, float x, float y, float z);
+string numberBinFile(string outputFileName, int b);
+
+#endif // MODULESTKACHENKO_H_INCLUDED
